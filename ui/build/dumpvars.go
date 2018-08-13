43e32d76f5b416a7f1ce4d08df368fa748ddb5af
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-13 11:03:48 -0700

    soong: Add AFTERLIFE_VERSION to dumpvars
    Change-Id: I24b29442f041aec451a173db404f542060ac67d2
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -139,6 +139,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
+	"AFTERLIFE_VERSION",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_BUILD_TYPE",
@@ -171,13 +172,19 @@ var BannerVars = []string{
 func Banner(make_vars map[string]string) string {
 	b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
+	fmt.Fprintln(b, "=======================================================")
+    fmt.Fprintln(b, " █████████████████████████████████████████████████████ ")
+	fmt.Fprintln(b, " ██▀▄─██▄─▄▄─█─▄─▄─█▄─▄▄─█▄─▄▄▀█▄─▄███▄─▄█▄─▄▄─█▄─▄▄─█ ")
+	fmt.Fprintln(b, " ██─▀─███─▄█████─████─▄█▀██─▄─▄██─██▀██─███─▄████─▄█▀█ ")
+	fmt.Fprintln(b, " ▀▄▄▀▄▄▀▄▄▄▀▀▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀ ")
+	fmt.Fprintln(b, "=======================================================")
+
 	for _, name := range BannerVars {
 		if make_vars[name] != "" {
 			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
 		}
 	}
-	fmt.Fprint(b, "============================================")
+	fmt.Fprintln(b, "=======================================================")
 
 	return b.String()
 }
